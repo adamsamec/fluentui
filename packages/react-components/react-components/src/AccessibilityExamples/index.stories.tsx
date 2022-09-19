b2100a7dcb9baf7406bc444661229d1191e42e81
Milan Turon <milanturon@centrum.cz>
2022-09-19 17:27:36 +0200

    add 3rd  example

--- packages/react-components/react-components/src/AccessibilityExamples/index.stories.tsx
+++ packages/react-components/react-components/src/AccessibilityExamples/index.stories.tsx
@@ -1,5 +1,6 @@
 export { AvoidActionExample } from './avoidAction.stories';
 export { AvoidComponentTypeExample } from './avoidComponentType.stories';
+export { AvoidStateExample } from './avoidState.stories';
 
 export default {
   title: 'Concepts/Developer/Accessibility/AccessibilityExamples',
