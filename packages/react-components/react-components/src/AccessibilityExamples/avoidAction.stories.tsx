b5a09f2b315b6713e2b892b86b1519d6bc842bec
Milan Turon <milanturon@centrum.cz>
2022-09-19 15:56:47 +0200

    fix navigation

--- packages/react-components/react-components/src/AccessibilityExamples/avoidAction.stories.tsx
+++ packages/react-components/react-components/src/AccessibilityExamples/avoidAction.stories.tsx
@@ -1,26 +1,47 @@
 import * as React from 'react';
 
 import {
-  Accordion,
+  Button,
+  Text,
+  Divider,
+  CalendarMonth,
   AccordionHeader,
   AccordionItem,
   AccordionPanel,
-  Button,
   Input,
   Label,
   Radio,
   RadioGroup,
   Checkbox,
 } from '@fluentui/react-components';
+import { bundleIcon, CalendarMonthFilled, CalendarMonthRegular } from '@fluentui/react-icons';
 
 import { Scenario } from './utils';
 
 export const AvoidActionExample = () => {
-  return (
-    <Scenario pageTitle="Personal form accordion">
-      <h1>Bad example</h1>
+  const CalendarMonth = bundleIcon(CalendarMonthFilled, CalendarMonthRegular);
 
-      <h1>Good example</h1>
+  return (
+    <Scenario pageTitle="Avoid action in accessibility name">
+      <h1>Avoid action in accessibility name</h1>
+
+      <h2>Bad example</h2>
+      <Button aria-label="press enter key to open calendar button" size="small" icon={<CalendarMonth />}>
+        Calendar
+      </Button>
+      <Text block>
+        <Text weight="semibold">Screen reader narraton:</Text> "press enter key to open calendar button"
+      </Text>
+      <Divider />
+      <h2>Good example</h2>
+      <Button size="small" icon={<CalendarMonth />}>
+        Calendar
+      </Button>
+      <Text block>
+        <Text weight="semibold">Screen reader narraton:</Text> "calendar button"
+      </Text>
+      <Divider />
+      <h2>User impact</h2>
     </Scenario>
   );
 };
