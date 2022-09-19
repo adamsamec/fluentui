9f850cb14b771f843be945ca15de769048702a01
Milan Turon <milanturon@centrum.cz>
2022-09-19 16:44:08 +0200

    adding another example

---
+++ packages/react-components/react-components/src/AccessibilityExamples/avoidComponentType.stories.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+import { Button, Text, Divider } from '@fluentui/react-components';
+import { bundleIcon, CalendarMonthFilled, CalendarMonthRegular } from '@fluentui/react-icons';
+
+import { Scenario } from './utils';
+
+export const AvoidComponentTypeExample = () => {
+  const CalendarMonth = bundleIcon(CalendarMonthFilled, CalendarMonthRegular);
+
+  return (
+    <Scenario pageTitle="Avoid component type in accessibility name">
+      <h1>Avoid component type in accessibility name</h1>
+
+      <h2>Bad example</h2>
+      <Button aria-label="Click here to open calendar " size="small" icon={<CalendarMonth />}>
+        Calendar
+      </Button>
+      <Text block>
+        <Text weight="semibold">Screen reader narration:</Text> "Click here to open calendar button"
+      </Text>
+      <Divider />
+      <h2>Good example</h2>
+      <Button size="small" icon={<CalendarMonth />}>
+        Calendar
+      </Button>
+      <Text block>
+        <Text weight="semibold">Screen reader narration:</Text> "calendar button"
+      </Text>
+      <Divider />
+      <h2>Problem explanation</h2>
+      <Text block>
+        Instruction on action prolong screen reader narration. It not neccessary information for sceen reader user. This
+        information is already know based on type of component which is used.
+      </Text>
+    </Scenario>
+  );
+};
--- packages/react-components/react-components/src/AccessibilityExamples/index.stories.tsx
+++ packages/react-components/react-components/src/AccessibilityExamples/index.stories.tsx
@@ -1,4 +1,5 @@
 export { AvoidActionExample } from './avoidAction.stories';
+export { AvoidComponentTypeExample } from './avoidComponentType.stories';
 
 export default {
   title: 'Concepts/Developer/Accessibility/AccessibilityExamples',
