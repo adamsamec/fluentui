a25a9fa43657c019fd3d8b3419e44027dd798e6a
Milan Turon <milanturon@centrum.cz>
2022-09-19 16:15:37 +0200

    adding explanation section

--- packages/react-components/react-components/src/AccessibilityExamples/avoidAction.stories.tsx
+++ packages/react-components/react-components/src/AccessibilityExamples/avoidAction.stories.tsx
@@ -1,19 +1,6 @@
 import * as React from 'react';
 
-import {
-  Button,
-  Text,
-  Divider,
-  CalendarMonth,
-  AccordionHeader,
-  AccordionItem,
-  AccordionPanel,
-  Input,
-  Label,
-  Radio,
-  RadioGroup,
-  Checkbox,
-} from '@fluentui/react-components';
+import { Button, Text, Divider } from '@fluentui/react-components';
 import { bundleIcon, CalendarMonthFilled, CalendarMonthRegular } from '@fluentui/react-icons';
 
 import { Scenario } from './utils';
@@ -26,11 +13,11 @@ export const AvoidActionExample = () => {
       <h1>Avoid action in accessibility name</h1>
 
       <h2>Bad example</h2>
-      <Button aria-label="press enter key to open calendar button" size="small" icon={<CalendarMonth />}>
+      <Button aria-label="Click here to open calendar " size="small" icon={<CalendarMonth />}>
         Calendar
       </Button>
       <Text block>
-        <Text weight="semibold">Screen reader narraton:</Text> "press enter key to open calendar button"
+        <Text weight="semibold">Screen reader narration:</Text> "Click here to open calendar button"
       </Text>
       <Divider />
       <h2>Good example</h2>
@@ -38,10 +25,14 @@ export const AvoidActionExample = () => {
         Calendar
       </Button>
       <Text block>
-        <Text weight="semibold">Screen reader narraton:</Text> "calendar button"
+        <Text weight="semibold">Screen reader narration:</Text> "calendar button"
       </Text>
       <Divider />
-      <h2>User impact</h2>
+      <h2>Problem explanation</h2>
+      <Text block>
+        Instruction on action prolong screen reader narration. It not neccessary information for sceen reader user. This
+        information is already know based on type of component which is used.
+      </Text>
     </Scenario>
   );
 };
