6bc75498a7977cf45ea88efb9e283c4d34e72f73
Milan Turon <milanturon@centrum.cz>
2022-09-20 09:30:17 +0200

    updating 3rd scenario

--- packages/react-components/react-components/src/AccessibilityExamples/avoidState.stories.tsx
+++ packages/react-components/react-components/src/AccessibilityExamples/avoidState.stories.tsx
@@ -12,13 +12,13 @@ export const AvoidStateExample = () => {
       <h2>Bad example</h2>
       <TabList defaultSelectedValue="Files">
         <Tab value="Chat">Chat</Tab>
-        <Tab value="Files" aria-label="Files tab selected">
+        <Tab value="Files" aria-label="Files tab is active">
           Files
         </Tab>
         <Tab value="Activity">Activity</Tab>
       </TabList>
       <Text block>
-        <Text weight="semibold">Screen reader narration:</Text> " Files tab selected tab selected 2 of 3"
+        <Text weight="semibold">Screen reader narration:</Text> " Files tab is active tab selected 2 of 3"
       </Text>
       <Divider />
       <h2>Good example</h2>
@@ -33,10 +33,10 @@ export const AvoidStateExample = () => {
       <Divider />
       <h2>Problem explanation</h2>
       <ul>
+        <li>Adding custom state is not required, because this functionality should provide us the screen reader.</li>
         <li>
-          Adding an action instruction (such as "Click here to...") prolongs the narration by screen reader. It adds
-          unnecessary information for screen reader user which is already known to the user based on the type of
-          component beeing used, i.e. a button in this case.
+          If state of coponent is not narrated, then verify there is used "aria-checked" or "aria-selected" attribute.
+          See aria documentation for exact usage.
         </li>
       </ul>
     </Scenario>
