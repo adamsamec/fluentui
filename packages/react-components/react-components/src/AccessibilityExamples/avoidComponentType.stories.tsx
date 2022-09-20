d858e751f1766f3e5d12fb3843efba9acb7569d7
Milan Turon <milanturon@centrum.cz>
2022-09-20 09:44:14 +0200

    edit second example

--- packages/react-components/react-components/src/AccessibilityExamples/avoidComponentType.stories.tsx
+++ packages/react-components/react-components/src/AccessibilityExamples/avoidComponentType.stories.tsx
@@ -1,31 +1,32 @@
 import * as React from 'react';
 
 import { Button, Text, Divider } from '@fluentui/react-components';
-import { bundleIcon, CalendarMonthFilled, CalendarMonthRegular } from '@fluentui/react-icons';
+import { bundleIcon, CalendarMonthFilled, Mic24Regular } from '@fluentui/react-icons';
 
 import { Scenario } from './utils';
 
 export const AvoidComponentTypeExample = () => {
-  const CalendarMonth = bundleIcon(CalendarMonthFilled, CalendarMonthRegular);
-
   return (
     <Scenario pageTitle="Avoid component type in accessibility name">
       <h1>Avoid component type in accessibility name</h1>
-
       <h2>Bad example</h2>
-      <Button aria-label="Mute microphone button" size="small" icon={<CalendarMonth />}>
-        Calendar
-      </Button>
-      <Text block>
-        <Text weight="semibold">Screen reader narration:</Text> "Click here to open calendar button"
-      </Text>
-      <Divider />
-      <h2>Good example</h2>
-      <Button size="small" icon={<CalendarMonth />}>
+      <Button aria-label="Mute microphone button" size="small" icon={<Mic24Regular />}>
         Mute microphone
       </Button>
       <Text block>
-        <Text weight="semibold">Screen reader narration:</Text> "calendar button"
+        <Text weight="semibold">Added aria label:</Text> aria-label="Mute microphone button"
+      </Text>
+      <Text block>
+        <Text weight="semibold">Screen reader narration:</Text>
+        "Mute microphone button button"
+      </Text>
+      <Divider />
+      <h2>Good example</h2>
+      <Button size="small" icon={<Mic24Regular />}>
+        Mute microphone
+      </Button>
+      <Text block>
+        <Text weight="semibold">Screen reader narration:</Text> "Mute microphone button"
       </Text>
       <Divider />
       <h2>Problem explanation</h2>
